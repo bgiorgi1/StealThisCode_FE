ffe27902c06a3680e8186e667098c95221e0d98f
“bgiorgi1” <Brianna.giorgi@gmail.com>
2021-07-06 18:03:40 -0400

    update

--- src/components/CreateSnippet.js
+++ src/components/CreateSnippet.js
@@ -133,7 +133,7 @@ class CreateSnippet extends Component {
 
 console.log('data', data)
     axios
-      .post(REACT_APP_SERVER_URL+'/api/snippets', data, {headers:{Authorization:currentUser, 'Content-Type': 'Application/json'}})
+      .post(REACT_APP_SERVER_URL+'/api/snippets', data, {headers:{'Authorization':currentUser, 'Content-Type': 'Application/json'}})
       .then(res => {
         console.log(res.data)
         this.setState({
